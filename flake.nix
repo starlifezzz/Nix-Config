9f9fb4a4f2510f1dcdace79db1dd170c730e44f3
zhangchongjie <778280151@qq.com>
2026-04-01 16:57:35 +0800

    ``` chore(flake): 添加 flake.lock 文件并注释备用镜像源
    添加了 flake.lock 锁定文件以确保依赖版本一致性，
    同时在 flake.nix 中注释了 USTC 镜像源作为备用选项。
    ```

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "https://mirrors.ustc.edu.cn/nix-channels/nixos-unstable/nixexprs.tar.xz";
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
