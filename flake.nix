be6243876c1e37dd34767d71be1751f314e30211
zhangchongjie <778280151@qq.com>
2026-04-01 12:47:29 +0800

    ``` feat(flake): 添加国内镜像源配置以加速 Nix 包管理
    - 在 flake.nix 中添加中科大和上交大镜像源配置
    - 配置 Nix 使用国内镜像源以解决下载速度慢的问题
    - 添加 nix.conf 配置文件包含镜像源和并行下载设置
    - 解决 GitHub 连接问题和提升包管理效率
    ```

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "NixOS configuration - Simple manual hardware selection";
 
   inputs = {
+    # 使用国内镜像源加速访问
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,6 +15,17 @@
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
     let
       system = "x86_64-linux";
+      
+      # 配置 Nix 使用国内镜像源
+      nixConfig = {
+        extra-substituters = [
+          "https://mirrors.ustc.edu.cn/nix-channels/store"
+          "https://mirror.sjtu.edu.cn/nix-channels/store"
+        ];
+        extra-trusted-public-keys = [
+          "nix-cache.nixos.org:/S8Ab5KwBhUd0RbHjOqg+2qdGkCv+o3yD7aWfL+QZ9xY="
+        ];
+      };
       
       pkgs-unstable = import nixpkgs-unstable {
         inherit system;
@@ -38,6 +51,17 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.extraSpecialArgs = { inherit self pkgs-unstable; };
+              
+              # Nix 下载配置
+              nix.settings = {
+                substituters = [
+                  "https://mirrors.ustc.edu.cn/nix-channels/store"
+                  "https://mirror.sjtu.edu.cn/nix-channels/store"
+                ];
+                trusted-public-keys = [
+                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                ];
+              };
             }
           ];
         };
